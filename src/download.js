acc555005dd8722a1d45947e3cf05ff08b2c4ded
lucas-oliveira17 <lucas_oliveira_viana@hotmail.com>
2021-05-11 21:14:09 -0300

    Fix logs and formatting

--- src/download.js
+++ src/download.js
@@ -1,57 +1,60 @@
-import axios from 'axios'
-import fs from 'fs'
+import axios from "axios";
+import fs from "fs";
 
-const CLIENT_ID = 'xg1wz0opfxtq3nkfr1ttjq5rafe2rx' 
+const CLIENT_ID = "xg1wz0opfxtq3nkfr1ttjq5rafe2rx";
 
 const PARAMETERS = {
-    game: 'League of Legends',
-    period: 'day',
-    limit: 20
-}
+  game: "League of Legends",
+  period: "day",
+  limit: 20,
+};
 
-const URLClipsLast24HoursByGame = `https://api.twitch.tv/kraken/clips/top?period=${PARAMETERS.period}&trending=true&limit=${PARAMETERS.limit}&game=${PARAMETERS.game}&language=pt-br`
-
-console.log({ URL_CLIPS: URLClipsLast24HoursByGame })
+const URLClipsLast24HoursByGame = `https://api.twitch.tv/kraken/clips/top?
+    period=${PARAMETERS.period}&
+    trending=true&limit=${PARAMETERS.limit}&
+    game=${PARAMETERS.game}&
+    language=pt-br`;
 
 async function download() {
-    const clips = (await axios.get(URLClipsLast24HoursByGame, {
-        headers: {
-            'Client-ID': CLIENT_ID,
-            'Accept': 'application/vnd.twitchtv.v5+json'
-        }
-    })).data.clips
+  const clips = (
+    await axios.get(URLClipsLast24HoursByGame, {
+      headers: {
+        "Client-ID": CLIENT_ID,
+        Accept: "application/vnd.twitchtv.v5+json",
+      },
+    })
+  ).data.clips;
 
-    const videosURLS = clips.map(clip => clip.thumbnails.medium.replace('-preview-480x272.jpg', '.mp4'));
+  const videosURLS = clips.map((clip) =>
+    clip.thumbnails.medium.replace("-preview-480x272.jpg", ".mp4")
+  );
 
-    await Promise.all(
-        videosURLS.map(url => { 
-            return axios.get(url, { responseType: 'stream' }) 
+  await Promise.all(
+    videosURLS.map((url) => {
+      return axios.get(url, { responseType: "stream" });
+    })
+  ).then(async (responses) => {
+    const fileNames = await Promise.all(
+      responses.map(async (response) => {
+        return new Promise((resolve, reject) => {
+          if (!fs.existsSync("videos")) {
+            fs.mkdirSync("videos");
           }
-        ))
-        .then(async responses => {
-            const fileNames = await Promise.all(responses.map(async response => {
-                
-                return new Promise((resolve, reject) => {
 
-                    if (!fs.existsSync("videos")){
-                        fs.mkdirSync("videos");
-                    }
+          const path = `videos/${removeTwitchVideoPrefix(response.config.url)}`;
 
-                    const path = `videos/${removeTwitchVideoPrefix(response.config.url)}`;
-
-                    response.data.pipe(fs.createWriteStream(path))
-                        .on('finish', () => {
-                            resolve(path)
-                            console.log('Video Downloaded!')
-                        });
-                })
-            }))
-        })
+          response.data.pipe(fs.createWriteStream(path)).on("finish", () => {
+            resolve(path);
+          });
+        });
+      })
+    );
+  });
 }
 
 function removeTwitchVideoPrefix(url) {
-    const TWITCH_PREFIX = 'https://clips-media-assets2.twitch.tv/';
-    return url.replace(TWITCH_PREFIX, '');
+  const TWITCH_PREFIX = "https://clips-media-assets2.twitch.tv/";
+  return url.replace(TWITCH_PREFIX, "");
 }
 
 download();
